ea697ed847ba116dc6fd1f54c1132922bdeceb50
SeongGyu <91594830+seonggyujo@users.noreply.github.com>
2025-04-19 23:50:30 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.c
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -4,7 +4,11 @@ int main()
 {
     int y;
     scanf("%d", &y);
-    if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) printf("1\n"); // 윤년
-    else printf("0\n");                                              // 평년
+
+    if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
+        printf("1\n");  // 윤년
+    else
+        printf("0\n");  // 평년
+
     return 0;
 }
